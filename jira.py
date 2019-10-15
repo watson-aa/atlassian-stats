641b4f8963a69e52c39121c1ea0639abcb8e9981
Aaron Watson <aaron@Aarons-MBP-010176017016.us.royalahold.net>
2019-10-15 14:11:38 -0400

    misc fixes

--- jira.py
+++ jira.py
@@ -19,16 +19,59 @@ class Jira:
         for project in self.__config['projects']:
             self._getDataFromApi(project, self.__config['projects'][project], start_date) 
 
-    def _getDataFromApi(self, project_name, project, start_date):
+    def _getDataFromApi(self, project, project_name, start_date):
         earliest_date = datetime.datetime.now()
-        counter = 0
 
-        url = 'https://' + self.__config['server']['Domain'] + self.__config['server']['ApiPath'] + project_name + '?jql=status%20%3D%20Done%20AND%20project%20%3D%20'
-        print(url)
+        conn = db.Connection(self.__config['database'])
+ 
+        url = 'https://' + self.__config['server']['Domain'] + self.__config['server']['ApiPath'] + '?jql=status%20%3D%20Done%20AND%20project%20%3D%20' + project
         auth = (self.__config['server']['User'], self.__config['server']['Password'])
         data = common.makeApiRequest(url, auth, False)
 
-        print(data)
+        (issues, accounts) = self.__extractIssueData(project, project_name, data)
+
+        conn.addUsers(accounts)
+        conn.addIssues(issues)
+        conn.closeConnection()
+
+    def __addAccount(self, account, accounts):
+        if account is None:
+            return (None, accounts)
+
+        tmp_account = {
+            'slug': account['key'],
+            'name': account['name'],
+            'emailAddress': account['emailAddress'],
+            'displayName': account['displayName'],
+            'type': 'NORMAL'
+        }
+        accounts[tmp_account['slug']] = tmp_account
+        return (account, accounts)
+
+    def __extractIssueData(self, project, project_name, data):
+        issues = []
+        accounts = {}
+        for issue in data['issues']:
+            (assignee, accounts) = self.__addAccount(issue['fields']['assignee'], accounts)
+            (creator, accounts) = self.__addAccount(issue['fields']['creator'], accounts)
+            (reporter, accounts) = self.__addAccount(issue['fields']['reporter'], accounts)
+            issues.append({
+                'issue_key': issue['id'],
+                'priority': issue['fields']['priority']['id'],
+                'status': issue['fields']['status']['name'],
+                'assignee': assignee['key'] if assignee is not None else None,
+                'creator': creator['key'] if creator is not None else None,
+                'reporter': reporter['key'] if reporter is not None else None,
+                'type': issue['fields']['issuetype']['name'],
+                'project': project,
+                'project_name': project_name,
+                'created_date': issue['fields']['created'],
+                'updated_date': issue['fields']['updated'],
+                'resolved_date': issue['fields']['resolutiondate'],
+                'description': issue['fields']['description'],
+                'summary': issue['fields']['summary']
+            })
+        return (issues, accounts)
 
 if __name__ == '__main__':
     jira = Jira()
